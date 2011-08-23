548a362c4e796fde1111d223e3568d22b53086e9
Brad Reid <brad.reid@canadadrugs.com>
2011-08-23 09:15:56 -0500

    add topbar nav

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,23 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function(){
+
+
+  // Dropdown example for topbar nav
+  // ===============================
+
+  $("body").bind("click", function (e) {
+    $('a.menu').parent("li").removeClass("open");
+    $('.menu-dropdown').hide();
+  });
+
+  $("a.menu").click(function (e) {
+    var li = $(this).parent("li");
+    li.toggleClass('open');
+    $('.menu-dropdown').show();
+    return false;
+  });
+
+
+});
